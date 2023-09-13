ab7c26ea268f6793fa25e89875a5f6982d6092bd
Udayvarma-1 <uday1bandu123@gmail.com>
2023-09-13 20:13:01 +0530

    formgroupnestedformgroup

--- src/app/createschool/createschool.component.ts
+++ src/app/createschool/createschool.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-createschool',
@@ -6,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./createschool.component.css']
 })
 export class CreateschoolComponent {
+
+  // FormGroup Method
+
+  public allstudentsForm:FormGroup = new FormGroup({
+    name:new FormControl(),
+    id:new FormControl(),
+    percentage:new FormControl(),
+    class:new FormControl(),
+    email:new FormControl(),
+    password:new FormControl(),
+
+    // nested FormGroup Method
+    address:new FormGroup({
+      hno:new FormControl(),
+      state:new FormControl(),
+      pin:new FormControl(),
+    })
+
+  });
+  submit(){
+    console.log(this.allstudentsForm)
+  }
 
 }
