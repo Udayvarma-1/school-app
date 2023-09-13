661bc0463a35bfc480c4f42536903611984431eb
Udayvarma-1 <uday1bandu123@gmail.com>
2023-09-13 20:55:18 +0530

    formarraymethod

--- src/app/createschool/createschool.component.ts
+++ src/app/createschool/createschool.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-createschool',
@@ -23,9 +23,37 @@ export class CreateschoolComponent {
       hno:new FormControl(),
       state:new FormControl(),
       pin:new FormControl(),
-    })
+    }),
+    type:new FormControl(),
+    busfee:new FormControl(),
+    hostelfee:new FormControl(),
 
+    // Formarray Mothed
+
+    cards:new FormArray([])
   });
+
+  get cardsFormArray(){
+    return this.allstudentsForm.get('cards') as FormArray;
+  }
+  addCard(){
+    this.cardsFormArray.push(
+      new FormControl({
+        no:new FormControl(),
+        exp:new FormControl(),
+        cvv:new FormControl()
+      })
+    )
+  }
+  deletecard(i:number){
+    this.cardsFormArray.removeAt(i);
+  }
+
+
+
+
+
+
   submit(){
     console.log(this.allstudentsForm)
   }
